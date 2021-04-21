0a7e4e294d484383ef7fd4f8503fef9ba3be7eab
Modar Halimeh <modr.naj@gmail.com>
2021-04-21 18:31:27 +0200

    Overhaul update
    Added a more explicit part for the truncated back-propagation. 
    Added explicit function to reset internal GRU states.

--- source_code/model/git_complexPF.py
+++ source_code/model/git_complexPF.py
@@ -1,5 +1,6 @@
 from git_config import *
 from model.istft import ISTFT
+import torch.optim.lr_scheduler as lrSched
 
 # Complex CNN for online processing.
 # Written for: Microsoft AEC Challenge 2021
@@ -54,7 +55,7 @@ class Decoder(nn.Module): #describes one decoder layer
         return xr, xi
 
 class ComplexGRU(nn.Module):
-    def __init__(self, inputSize= 64*17*2, hiddenSize= 128, numLayers =1):
+    def __init__(self, inputSize= 64*17*2, hiddenSize= 128, numLayers =1, batchSize=1):
         super(ComplexGRU, self).__init__()
 
         self.hiddenSize = hiddenSize
@@ -66,31 +67,42 @@ class ComplexGRU(nn.Module):
         self.FCLi      = torch.nn.Linear(in_features=hiddenSize, out_features=inputSize, bias=False)
         self.act       = nn.LeakyReLU(0.1)
 
+        self.register_buffer('hrrOInit', torch.zeros(1, batchSize, hiddenSize))
+
+        self.hrrO, self.hiiO, self.hirO, self.hriO =  self.hrrOInit[:, 0:batchSize,:], self.hrrOInit[:, 0:batchSize,:], self.hrrOInit[:, 0:batchSize,:], self.hrrOInit[:, 0:batchSize,:]
+
+    def reset_internal_stat(self, batchSize = 1, Device=device):
+
+        self.register_buffer('hrrOInit', torch.zeros(1, batchSize, self.hiddenSize))
+
+        if Device.type=='cuda':
+            self.hrrOInit = self.hrrOInit.cuda()
+
+        self.hrrO, self.hiiO, self.hirO, self.hriO =  self.hrrOInit[:, 0:batchSize,:], self.hrrOInit[:, 0:batchSize,:], self.hrrOInit[:, 0:batchSize,:], self.hrrOInit[:, 0:batchSize,:]
+
+
     def forward(self, xr, xi):
 
         originalShape = xr.shape
 
-        xr = torch.reshape(xr, [-1, originalShape[3], originalShape[2]*originalShape[1]])
-        xi = torch.reshape(xi, [-1, originalShape[3], originalShape[2]*originalShape[1]])
+        xr = torch.reshape(xr, [-1, 1, originalShape[3]*originalShape[2]*originalShape[1]])
+        xi = torch.reshape(xi, [-1, 1, originalShape[3]*originalShape[2]*originalShape[1]])
 
-        GRU_rr, _ = self.grulayerR(xr)
-        GRU_ri, _ = self.grulayerI(xr)
+        GRU_rr, self.hrrO = self.grulayerR(xr, self.hrrO)
+        GRU_ri, self.hriO = self.grulayerI(xr, self.hriO)
+
+        GRU_ii, self.hiiO = self.grulayerI(xi, self.hiiO)
+        GRU_ir, self.hirO = self.grulayerR(xi, self.hirO)
 
 
-        GRU_ii, _ = self.grulayerI(xi)
-        GRU_ir, _ = self.grulayerR(xi)
+        xr_gru = GRU_rr - GRU_ii
+        xi_gru = GRU_ir + GRU_ri
 
-
-        xr = GRU_rr - GRU_ii
-        xi = GRU_ir + GRU_ri
-
-        xr = self.FCLr(xr) - self.FCLi(xi)
-        xi = self.FCLi(xr) + self.FCLr(xi)
+        xr = self.FCLr(xr_gru) - self.FCLi(xi_gru)
+        xi = self.FCLi(xr_gru) + self.FCLr(xi_gru)
 
         xr, xi = torch.reshape(xr, [originalShape[0], originalShape[1], originalShape[2],
-                                          originalShape[3]]), torch.reshape(xi,
-                                                                            [originalShape[0], originalShape[1],
-                                                                             originalShape[2], originalShape[3]])
+                                          originalShape[3]]), torch.reshape(xi, [originalShape[0], originalShape[1], originalShape[2], originalShape[3]])
         return xr, xi
 
 
@@ -100,17 +112,18 @@ class complexPF(pl.LightningModule):
         self.encoders = nn.ModuleList()
         for layer in range(0, 4):
             self.encoders.append(Encoder(cfg['encoders'][layer], 0.1))
+
         self.decoders = nn.ModuleList()
         for layer in range(0, 3):
             self.decoders.append(Decoder(cfg['decoders'][layer], 0.1))
 
-        self.decoders.append(complexnn.ComplexConvWrapper(nn.ConvTranspose2d,
-                *cfg['decoders'][3], bias=True))
+        self.final_dec = complexnn.ComplexConvWrapper(nn.ConvTranspose2d, *cfg['decoders'][3], bias=True)
 
-        self.gruLayer = ComplexGRU(inputSize=cfg['GRUdim'], hiddenSize=cfg['GRUdim'], numLayers=1)
+        self.gruLayer = ComplexGRU(inputSize=cfg['GRUdim'], hiddenSize=cfg['GRUdim'], numLayers=1, batchSize=1)
 
-        self.hop_length = cfg['hop_length']
-        self.nrFrames = cfg['nrFrames']
+        self.hop_length         = cfg['hop_length']
+        self.nrFrames           = cfg['nrFrames']
+        self.contextLength      = cfg['memoryLength']
 
     def get_ratio_mask(self, o_real, o_imag):
         mag = torch.sqrt(o_real ** 2 + o_imag ** 2)
@@ -123,54 +136,75 @@ class complexPF(pl.LightningModule):
         phase = phase + torch.atan2(xi, xr)
         return mag * torch.cos(phase), mag * torch.sin(phase)
 
-    def forward(self, xr, xi):
-        input_real, input_imag = xr, xi
-        skips = list()
-        for encoder in self.encoders:
-            xr, xi = encoder(xr, xi)
-            skips.append((xr, xi))
+    def forward(self, xr, xi, runFlag):
 
-        xr, xi = self.gruLayer(xr, xi)
+        if runFlag:
+            input_real, input_imag = xr, xi
+            skips = list()
+            for encoder in self.encoders:
+                xr, xi = encoder(xr, xi)
+                skips.append((xr, xi))
 
-        skip = skips.pop()
-        skip = None
-        for decoder in self.decoders[:-1]:
-            xr, xi = decoder(xr, xi, skip)
+            xr, xi = self.gruLayer(xr, xi)
+
             skip = skips.pop()
+            skip = None
+            for decoder in self.decoders:
+                xr, xi = decoder(xr, xi, skip)
+                skip = skips.pop()
 
-        xr, xi = padded_cat(xr, skip[0], dim=1), padded_cat(xi, skip[1],
-                                                            dim=1)  # ensures skip connection sizes are compatible
-        xr, xi = self.decoders[-1](xr, xi)
+            xr, xi = padded_cat(xr, skip[0], dim=1), padded_cat(xi, skip[1],
+                                                                dim=1)  # ensures skip connection sizes are compatible
+            xr, xi = self.final_dec(xr, xi)
 
-        input_real = input_real[:, 0, :, :].unsqueeze(dim=1)
-        input_imag = input_imag[:, 0, :, :].unsqueeze(dim=1)
+            input_real = input_real[:, 0, :, :].unsqueeze(dim=1)
+            input_imag = input_imag[:, 0, :, :].unsqueeze(dim=1)
 
-        xr, xi = pad2d_as(xr, input_real), pad2d_as(xi, input_imag)
-        mag_mask, phase_corr = self.get_ratio_mask(xr, xi)
+            xr, xi = pad2d_as(xr, input_real), pad2d_as(xi, input_imag)
+            mag_mask, phase_corr = self.get_ratio_mask(xr, xi)
 
-        return self.apply_mask(input_real, input_imag, mag_mask, phase_corr)
+            return self.apply_mask(input_real, input_imag, mag_mask, phase_corr)
+        else:
+            for encoder in self.encoders:
+                xr, xi = encoder(xr, xi)
+            xr, xi = self.gruLayer(xr, xi)
+            return 1
 
     def training_step(self, batch, batch_idx):
         farend, residual, source = batch
 
+        batchSize = len(farend[:, 0, 0])
+
+        self.gruLayer.reset_internal_stat(batchSize=batchSize, Device=self.device)
+
         farend_f    = stft(farend.squeeze()).unsqueeze(dim=1)
         residual_f  = stft(residual.squeeze()).unsqueeze(dim=1)
+        source_f    = stft(source.squeeze()).unsqueeze(dim=1)
 
         residual_f_r, farend_f_r = residual_f[..., 1:-1, 0], farend_f[..., 1:-1, 0]
         residual_f_i, farend_f_i = residual_f[..., 1:-1, 1], farend_f[..., 1:-1, 1]
+        source_f_r, source_f_i   = source_f[..., 1:-1, 0], source_f[..., 1:-1, 1]
 
-        xr, xi          = torch.cat((residual_f_r, farend_f_r), dim=1), torch.cat((residual_f_i, farend_f_i), dim=1)
-        out_r, out_i    = self(xr, xi)
+        for memoryInd in range(1, self.contextLength):
+            xr, xi = torch.cat((residual_f_r[..., memoryInd:memoryInd+2], farend_f_r[..., memoryInd:memoryInd+2]),
+                               dim=1), torch.cat((residual_f_i[..., memoryInd:memoryInd+2], farend_f_i[..., memoryInd:memoryInd+2]), dim=1)
+            _ = self.forward(xr, xi, runFlag=False)
+
+        xr, xi = torch.cat((residual_f_r[..., self.contextLength:self.contextLength+2], farend_f_r[..., self.contextLength:self.contextLength + 2]),
+                           dim=1), torch.cat((residual_f_i[..., self.contextLength:self.contextLength+2],
+                                              farend_f_i[..., self.contextLength:self.contextLength + 2]), dim=1)
+        out_r, out_i    = self.forward(xr, xi, runFlag=1)
         out_r, out_i = torch.squeeze(out_r, 1), torch.squeeze(out_i, 1)
         out_audio = istft(out_r, out_i)
-        out_audio = out_audio[:, :, -2 * self.hop_length:-self.hop_length].squeeze() # take only last two frames
+        out_audio = out_audio[:, :, -2*self.hop_length:-1*self.hop_length].squeeze() # take only last two frames
 
-        residual = residual.squeeze()
-        loss = utils.wSDRLoss(residual[:, -2*hop_length:-hop_length].squeeze(),
-                              source.squeeze(), out_audio.squeeze(), 2e-7)
+        residual = istft(residual_f_r[..., self.contextLength:self.contextLength+2].squeeze(dim=1), residual_f_i[..., self.contextLength:self.contextLength+2].squeeze(dim=1)).squeeze()
+        source = istft(source_f_r[..., self.contextLength:self.contextLength+2].squeeze(dim=1), source_f_i[..., self.contextLength:self.contextLength+2].squeeze(dim=1)).squeeze()
 
-        train_loss = loss.detach()
-        self.log('train_loss', train_loss, on_step=False, on_epoch=True, prog_bar=False)
+        loss = utils.wSDRLoss(residual[:, -2*self.hop_length:-1*self.hop_length].squeeze(),
+                              source[:, -2*self.hop_length:-1*self.hop_length].squeeze(), out_audio.squeeze(), 2e-8)
+
+        self.log('train_loss', loss, on_step=False, on_epoch=True, prog_bar=False)
 
         return loss
 
@@ -179,19 +213,40 @@ class complexPF(pl.LightningModule):
 
         farend_f    = stft(farend.squeeze()).unsqueeze(dim=1)
         residual_f  = stft(residual.squeeze()).unsqueeze(dim=1)
+        source_f    = stft(source.squeeze()).unsqueeze(dim=1)
 
-        residual_f_r, farend_f_r = residual_f[..., 0], farend_f[..., 0]
-        residual_f_i, farend_f_i = residual_f[..., 1], farend_f[..., 1]
+        batchSize = len(farend[:, 0, 0])
+        self.gruLayer.reset_internal_stat(batchSize=batchSize, Device=self.device)
 
-        xr, xi          = torch.cat((residual_f_r, farend_f_r), dim=1), torch.cat((residual_f_i, farend_f_i), dim=1)
-        out_r, out_i    = self(xr, xi)
+        residual_f_r, farend_f_r = residual_f[..., 1:-1, 0], farend_f[..., 1:-1, 0]
+        residual_f_i, farend_f_i = residual_f[..., 1:-1, 1], farend_f[..., 1:-1, 1]
+
+        nr_frames = len(residual_f[0, 0, 0, :, 0])
+
+        xr, xi = torch.cat((residual_f_r[..., 0:0 + 2], farend_f_r[..., 0:0 + 2]), dim=1), \
+                 torch.cat((residual_f_i[..., 0:0 + 2], farend_f_i[..., 0:0 + 2]), dim=1)
+
+        out_r, out_i = self(xr, xi, runFlag=1)
         out_r, out_i = torch.squeeze(out_r, 1), torch.squeeze(out_i, 1)
         out_audio = istft(out_r, out_i).squeeze()
+        out_audio = out_audio[:, -2*self.hop_length:-1*self.hop_length].squeeze()
 
-        source = source.squeeze()
+        for frameInd in range(1, nr_frames-3):
+            xr, xi = torch.cat((residual_f_r[..., frameInd:frameInd+2], farend_f_r[..., frameInd:frameInd+2]), dim=1), \
+                     torch.cat((residual_f_i[..., frameInd:frameInd+2], farend_f_i[..., frameInd:frameInd+2]), dim=1)
+            out_r, out_i    = self(xr, xi, runFlag=1)
+            out_r, out_i    = torch.squeeze(out_r, 1), torch.squeeze(out_i, 1)
+            out_audio_tmp   = istft(out_r, out_i).squeeze()
+            out_audio_tmp   = out_audio_tmp[:, -2*self.hop_length:-1*self.hop_length].squeeze()
+            out_audio       = torch.cat((out_audio, out_audio_tmp), dim=1)
+
+        source = istft(source_f[..., 1:-1, 0].squeeze(dim=1), source_f[..., 1:-1, 1].squeeze(dim=1)).squeeze()
+        residual = istft(residual_f_r.squeeze(dim=1), residual_f_i.squeeze(dim=1)).squeeze()
+
         L = np.minimum(len(out_audio[0, :]), len(source[0, :]))
 
-        loss = utils.wSDRLoss(residual[:, :L].squeeze(), source[:, :L].squeeze(), out_audio[:, :L].squeeze(), 2e-7)
+        loss = utils.wSDRLoss(residual[:, :L].squeeze(), source[:, :L].squeeze(), out_audio[:, :L].squeeze(), 2e-8)
+
 
         self.log('val_loss', loss.detach(), on_step=False, on_epoch=True, prog_bar=False)
 
@@ -201,17 +256,34 @@ class complexPF(pl.LightningModule):
         except:
             farend, residual, item_name = batch
 
+        batchSize = len(farend[:, 0, 0])
+        self.gruLayer.reset_internal_stat(batchSize=batchSize, Device=self.device)
 
         farend_f = stft(farend.squeeze()).unsqueeze(dim=1)
         residual_f = stft(residual.squeeze()).unsqueeze(dim=1)
 
-        residual_f_r, farend_f_r = residual_f[..., 0], farend_f[..., 0]
-        residual_f_i, farend_f_i = residual_f[..., 1], farend_f[..., 1]
+        residual_f_r, farend_f_r = residual_f[..., 1:-1, 0], farend_f[..., 1:-1, 0]
+        residual_f_i, farend_f_i = residual_f[..., 1:-1, 1], farend_f[..., 1:-1, 1]
 
-        xr, xi = torch.cat((residual_f_r, farend_f_r), dim=1), torch.cat((residual_f_i, farend_f_i), dim=1)
-        out_r, out_i = self(xr, xi)
+        nr_frames = len(residual_f[0, 0, 0, :, 0])
+
+        xr, xi = torch.cat((residual_f_r[..., 0:0 + 2], farend_f_r[..., 0:0 + 2]), dim=1), \
+                 torch.cat((residual_f_i[..., 0:0 + 2], farend_f_i[..., 0:0 + 2]), dim=1)
+        out_r, out_i = self(xr, xi, runFlag=1)
         out_r, out_i = torch.squeeze(out_r, 1), torch.squeeze(out_i, 1)
-        out_audio = istft(out_r, out_i)
+        out_audio = istft(out_r, out_i).squeeze()
+        out_audio = out_audio[:, -2 * self.hop_length:-1 * self.hop_length].squeeze()
+
+        for frameInd in range(1, nr_frames - 3):
+            xr, xi = torch.cat((residual_f_r[..., frameInd:frameInd + 2], farend_f_r[..., frameInd:frameInd + 2]),
+                               dim=1), \
+                     torch.cat((residual_f_i[..., frameInd:frameInd + 2], farend_f_i[..., frameInd:frameInd + 2]),
+                               dim=1)
+            out_r, out_i = self(xr, xi, runFlag=1)
+            out_r, out_i = torch.squeeze(out_r, 1), torch.squeeze(out_i, 1)
+            out_audio_tmp = istft(out_r, out_i).squeeze()
+            out_audio_tmp = out_audio_tmp[:, -2 * self.hop_length:-1 * self.hop_length].squeeze()
+            out_audio = torch.cat((out_audio, out_audio_tmp), dim=1)
 
         nrSamples = len(item_name)
         for sampleId in range(0, nrSamples):
@@ -222,10 +294,11 @@ class complexPF(pl.LightningModule):
             scipy.io.savemat('./results/'+file+'.mat', sig)
 
 
-
     def configure_optimizers(self):
-        optimizer= Adam(self.parameters(), lr=1e-3) # experiment with lr, you can add a scheduler too
-        return [optimizer]
+        optimizer= Adam(self.parameters(), lr=1e-3) # 6e-4
+        lrLambda = lambda epoch: 0.95**epoch
+        scheduler= lrSched.LambdaLR(optimizer, lrLambda)
+        return [optimizer], [scheduler]
 
 
 
